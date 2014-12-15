b88f32f31b1257814269c57b5065f844cd918dd0
lord349123 <t-ho@web.de>
2014-12-15 20:19:09 +0100

    Es gibt jetzt die Klasse Block und ein par Unterklassen, allerdings muss deren Funktionsumfang später festgelegt werden. Dazu gbt es jetzt auch die Klasse Struktur.

---
+++ Lands of Oblvion/src/blöcke/Eichenbrettschräge.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blöcke;
+
+/**
+ *
+ * @author To
+ */
+public class Eichenbrettschräge extends Block{
+    
+}
---
+++ Lands of Oblvion/src/strukturen/Struktur.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package strukturen;
+
+/**
+ *
+ * @author To
+ */
+public class Struktur {
+    
+}
